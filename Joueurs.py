fb9bff4825f98c964cdac2742c11d70a4c0aa99a
ryan <ryanloureau@gmail.com>
2023-05-21 01:18:31 +0200

    Ajout des principaux fichiers

---
+++ Joueurs.py
@@ -0,0 +1,42 @@
+# Classe pour un Joueur
+
+class Joueur:
+
+    def __init__(self, nom, prenom, taille, poids, poste, vitesse, agilite, force, precision, endurance, moral, sante, equipe):
+
+        # Attributs principaux
+        self.nom = nom
+        self.prenom = prenom
+        self.taille = taille
+        self.poids = poids
+        self.poste = poste
+        self.vitesse = vitesse
+        self.agilite = agilite
+        self.force = force
+        self.precision = precision
+        self.endurance = endurance
+        self.moral = moral
+        self.sante = sante
+        self.equipe = equipe
+
+        # Attributs secondaires
+        self.vitesse_moyenne = (self.vitesse + self.agilite) / 2
+        self.precision_moyenne = (self.precision + self.agilite) / 2
+        self.endurance_moyenne = (self.endurance + self.force) / 2
+        self.moral_moyen = (self.moral + self.sante) / 2
+
+        # Attributs de jeu
+        self.score = 0
+        self.passe = 0
+        self.rebond = 0
+        self.interception = 0
+        self.contre = 0
+        self.faute = 0
+        self.faute_technique = 0
+        self.faute_flagrante = 0
+        self.perte_de_balle = 0
+        self.tir_a_2_points = 0
+        self.tir_a_3_points = 0
+        self.tir_a_2_points_reussi = 0
+        self.tir_a_3_points_reussi = 0
+
---
+++ SImulation.py
@@ -0,0 +1,36 @@
+# SImulation d'une partie de basket avec 2 équipes de 5 joueurs
+# Le fichier permet l'affichage graphique de la simulation
+
+
+from pygame import*
+
+# Initialisation de la fenêtre par une fonction
+def init_fenetre():
+    fenetre = display.set_mode((1820,980))
+    display.set_caption("Basketball")
+
+    return fenetre
+
+# Fonction de boucle de jeu
+def boucle_jeu(fenetre):
+    # Initialisation des variables
+    fond = image.load("Data/Background.jpg").convert()
+    # On adapte l'image de fond à la taille de la fenêtre
+    fond = transform.scale(fond,(1820,980))
+    fenetre.blit(fond,(0,0))
+    display.flip()
+    continuer = True
+    # Boucle de jeu
+    while continuer:
+        for evenement in event.get():
+            if evenement.type == QUIT:
+                continuer = False
+        display.flip()
+    quit()
+
+# Fonction principale
+def main():
+    fenetre = init_fenetre()
+    boucle_jeu(fenetre)
+
+main()
